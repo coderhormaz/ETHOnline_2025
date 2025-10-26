3caba671e30dd160096c625f4c149ab8f09debb7
coderhormaz <hormazdaruwala86@gmail.com>
2025-10-26 19:00:53 +0530

    v19

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,17 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    port: 4173,
+    strictPort: true,
+  },
+  server: {
+    port: 5173,
+    strictPort: true,
+    host: true,
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+  },
 })
